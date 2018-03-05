fbb0f04b2a666c083b8aa56e80ab66cbddbc643f
SamarV-121 <samarvispute121@gmail.com>
2018-03-05 11:41:35 +0530

    nicklaus: Initial Commit

---
+++ BoardConfig.mk
@@ -0,0 +1,67 @@
+#
+# Copyright 2017 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a53
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6737m
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := mt6737m
+TARGET_NO_BOOTLOADER := true
+# TARGET_NO_FACTORYIMAGE := true
+
+# Filesystems
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+# Kernel
+BOARD_KERNEL_IMAGE_NAME := zImage-dtb
+TARGET_KERNEL_SOURCE := kernel/motorola/nicklaus
+TARGET_KERNEL_CONFIG := lineageos_nicklaus_defconfig
+BOARD_KERNEL_BASE := 0x40000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_RAMDISK_OFFSET := 0x04000000
+BOARD_TAGS_OFFSET := 0xE000000
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,32N2 androidboot.selinux=permissive
+BOARD_KERNEL_OFFSET := 0x00008000
+BOARD_MKBOOTIMG_ARGS := --kernel_offset $(BOARD_KERNEL_OFFSET) --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_TAGS_OFFSET)
+KERNEL_TOOLCHAIN_PREFIX := arm-linux-androidkernel-
+
+# Storage Allocations
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+# TWRP Flags
+RECOVERY_VARIANT := twrp
+TW_THEME := portrait_hdpi
+TW_EXCLUDE_SUPERSU := true
+TW_INCLUDE_INJECTTWRP := false
+TW_DEVICE_VERSION := SamarV-1213
+TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
+TW_INCLUDE_FB2PNG := true
+# TW_INCLUDE_FUSE_EXFAT := true
+# TW_INCLUDE_NTFS_3G := true
+TW_ALWAYS_RMRF := true
+TW_HAS_DOWNLOAD_MODE := false
+INTERNAL_LOCAL_CLANG_EXCEPTION_PROJECTS := external/busybox/
+TW_NO_SCREEN_BLANK := true
+TW_CUSTOM_BATTERY_PATH := "/sys/devices/platform/battery/power_supply/battery"
---
+++ full_nicklaus.mk
@@ -0,0 +1,44 @@
+#
+# Copyright 2017 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+## Specify phone tech before including full_phone
+
+# Release name
+PRODUCT_RELEASE_NAME := nicklaus
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    charger
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := nicklaus
+PRODUCT_NAME := full_nicklaus
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := Moto E4 Plus
+PRODUCT_MANUFACTURER := motorola
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+# Available languages
+PRODUCT_LOCALES := en_US en_GB ru_RU uk_UA tr_TR sk_SK vi_VN fr_FR ar_EG
